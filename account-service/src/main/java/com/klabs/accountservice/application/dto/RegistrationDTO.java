251589d253c40cc92b42570cd97fc63d8232d5ce
kzt18829d <kzt.kaedehara@gmail.com>
2025-11-24 16:00:50 +0300

    Fixup RegistrationDTO.java

--- account-service/src/main/java/com/klabs/accountservice/application/dto/RegistrationDTO.java
+++ account-service/src/main/java/com/klabs/accountservice/application/dto/RegistrationDTO.java
@@ -1,15 +1,11 @@
 package com.klabs.accountservice.application.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class RegistrationDTO {
     private String registrationID;
